f3e18414f1d7ae9f163c06525608c2b11a4c529a
JoshMJohnson <joshjohns21@gmail.com>
2023-06-21 23:44:47 -0500

    Workout Calendar exercise categories layout

--- WorkoutCalendar.xaml.cs
+++ WorkoutCalendar.xaml.cs
@@ -1,3 +1,5 @@
+using WorkoutLog.Model;
+
 namespace WorkoutLog;
 
 public partial class WorkoutCalendar : ContentPage
@@ -5,6 +7,8 @@ public partial class WorkoutCalendar : ContentPage
 	public WorkoutCalendar()
 	{
 		InitializeComponent();
+
+        Refresh_Categories();
     }
 
     /* button clicked to create a workout category */
@@ -17,5 +21,41 @@ public partial class WorkoutCalendar : ContentPage
     private async void Remove_Category(object sender, EventArgs e)
     {
         await DisplayAlert("Remove Category", "ya", "Remove");
+    }
+
+    /* updates category display */
+    private void Refresh_Categories()
+    {
+        List<Category> category_list = new List<Category>();
+
+        /* TESTING TEMP LIST start */
+        Category temp1 = new Category();
+        Category temp2 = new Category();
+        Category temp3 = new Category();
+        Category temp4 = new Category();
+
+        temp1.Id = 1;
+        temp1.Name = "Push";
+        temp1.Color = "red";
+
+        temp2.Id = 2;
+        temp2.Name = "Pull";
+        temp2.Color = "green";
+
+        temp3.Id = 3;
+        temp3.Name = "Legs";
+        temp3.Color = "blue";
+
+        temp4.Id = 4;
+        temp4.Name = "Running";
+        temp4.Color = "yellow";
+
+        category_list.Add(temp1);
+        category_list.Add(temp2);
+        category_list.Add(temp3);
+        category_list.Add(temp4);
+        /* TESTING TEMP LIST end */
+
+        workout_category_display.ItemsSource = category_list;
     }
 }
