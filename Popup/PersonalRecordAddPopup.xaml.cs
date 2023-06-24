bb5bc982e0993f8a63e24fa3b6240121e1e19d09
JoshMJohnson <joshjohns21@gmail.com>
2023-06-23 21:30:41 -0500

    created display difference for toggling pr type

--- Popup/PersonalRecordAddPopup.xaml.cs
+++ Popup/PersonalRecordAddPopup.xaml.cs
@@ -5,6 +5,8 @@ public partial class PersonalRecordAddPopup
 	public PersonalRecordAddPopup()
 	{
 		InitializeComponent();
+
+        time_display.IsVisible = false;
     }
 
     /* creates an exercise along with the PR */
@@ -17,5 +19,21 @@ public partial class PersonalRecordAddPopup
     private void Cancel_PR(object sender, EventArgs e)
     {
         Close();
+    }
+
+    /* handles event when toggled between weight and time PR type */
+    private void exercise_type_change(object sender, EventArgs e)
+    {
+        /* if changed to toggle on weight PR */
+        if (time_display.IsVisible)
+        {
+            weight_display.IsVisible = true;
+            time_display.IsVisible = false;
+        }
+        else /* else changed to toggle on time PR */
+        {
+            weight_display.IsVisible = false;
+            time_display.IsVisible = true;
+        }
     }
 }
