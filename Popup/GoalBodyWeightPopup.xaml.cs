8d82ed1f87474ee025ccaacb320f5b70520a3215
JoshMJohnson <joshjohns21@gmail.com>
2023-06-25 16:16:42 -0500

    Goal body weight popup filled

--- Popup/GoalBodyWeightPopup.xaml.cs
+++ Popup/GoalBodyWeightPopup.xaml.cs
@@ -18,4 +18,19 @@ public partial class GoalBodyWeightPopup
     {
         Close();
     }
+
+    /* handles event when toggled between goal date and no date */
+    private void date_included_change(object sender, EventArgs e)
+    {
+        if (date_included_toggle.IsToggled) /* if date desired */
+        {
+            record_date_display.IsVisible = true;
+            no_date_display.IsVisible = false;
+        }
+        else /* else no date desired */
+        {
+            record_date_display.IsVisible = false;
+            no_date_display.IsVisible = true;
+        }
+    }
 }
