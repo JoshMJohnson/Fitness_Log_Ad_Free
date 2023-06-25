0538ae90eb927692e12a9cee0279740e616dd8d3
JoshMJohnson <joshjohns21@gmail.com>
2023-06-24 19:16:52 -0500

    formatted goal pr popup

--- Popup/GoalPRPopup.xaml.cs
+++ Popup/GoalPRPopup.xaml.cs
@@ -22,12 +22,31 @@ public partial class GoalPRPopup
     /* handles event when toggled between weight and time PR type */
     private void exercise_type_change(object sender, EventArgs e)
     {
-        
+        /* if changed to toggle on weight PR */
+        if (time_display.IsVisible)
+        {
+            weight_display.IsVisible = true;
+            time_display.IsVisible = false;
+        }
+        else /* else changed to toggle on time PR */
+        {
+            weight_display.IsVisible = false;
+            time_display.IsVisible = true;
+        }
     }
 
     /* handles event when toggled between goal date and no date */
     private void date_included_change(object sender, EventArgs e)
     {
-
+        if (date_included_toggle.IsToggled) /* if date desired */
+        {
+            record_date_display.IsVisible = true;
+            no_date_display.IsVisible = false;
+        }
+        else /* else no date desired */
+        {
+            record_date_display.IsVisible = false;
+            no_date_display.IsVisible = true;
+        }
     }
 }
