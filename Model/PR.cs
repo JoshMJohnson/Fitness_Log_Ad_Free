7408be74ab92ce765dfcf585f1185696f1ee3bcf
JoshMJohnson <joshjohns21@gmail.com>
2023-06-29 00:41:07 -0500

    Update PR.cs

--- Model/PR.cs
+++ Model/PR.cs
@@ -10,11 +10,11 @@ public class PR
 
     public DateTime date_achieved { get; set; }
 
+    public bool weight_pr_type { get; set; } /* true = Weight; else false = Time */
+
     public int weight { get; set; }
 
     public int time_hours { get; set; }
     public int time_min { get;set; }
     public int time_sec { get; set; }
-
-    public bool weight_pr_type { get; set; } /* true = Weight; else false = Time */
 }
