5f977c7eeeba99ccdd6c59373fc02a5074f2cc2f
JoshMJohnson <joshjohns21@gmail.com>
2023-07-11 15:53:41 -0500

    pr update popup error prompt

--- Popup/UpdatePR.xaml.cs
+++ Popup/UpdatePR.xaml.cs
@@ -20,26 +20,78 @@ public partial class UpdatePR
 
         if (updating_pr.is_weight_pr == false) /* if time pr */
         {
-            string min_update_string = min_pr.Text.ToString();
-            int min_update = int.Parse(min_update_string);
+            string hr_update_string = hr_pr.Text;
+            string min_update_string = min_pr.Text;
+            string sec_update_string = sec_pr.Text;
 
-            string sec_update_string = sec_pr.Text.ToString();
-            int sec_update = int.Parse(sec_update_string);
+            if ((hr_update_string != null && hr_update_string.Length != 0) || (min_update_string != null && min_update_string.Length != 0)
+                    || (sec_update_string != null && sec_update_string.Length != 0)) /* if at least one time field is not empty */
+            {
+                int hr_update;
+                int min_update;
+                int sec_update;
 
-            int hours = min_update / 60;
-            int mins = min_update % 60;
+                if (hr_update_string != null && hr_update_string.Length != 0) /* if hour field is not empty */
+                {
+                    hr_update_string = hr_update_string.ToString();
+                    hr_update = int.Parse(hr_update_string);
+                }
+                else /* else; hr field is empty */
+                {
+                    hr_update = 0;
+                }
 
-            await App.RecordRepo.Update_PR(name, date, -1, hours, mins, sec_update);
+                if (min_update_string != null && min_update_string.Length != 0) /* if min field is not empty */
+                {
+                    min_update_string = min_update_string.ToString();
+                    min_update = int.Parse(min_update_string);
+                }
+                else /* else; min field is empty */
+                {
+                    min_update = 0;
+                }
+
+                if (sec_update_string != null && sec_update_string.Length != 0) /* if sec field is not empty */
+                {
+                    sec_update_string = sec_update_string.ToString();
+                    sec_update = int.Parse(sec_update_string);
+                }
+                else /* else; sec field is empty */
+                {
+                    sec_update = 0;
+                }
+
+                await App.RecordRepo.Update_PR(name, date, -1, hr_update, min_update, sec_update);
+
+                error_prompt.IsVisible = false;
+                Close();
+            }
+            else /* else; time field is empty */
+            {
+                error_prompt.Text = "At least one time field must have a value";
+                error_prompt.IsVisible = true;
+            }
         }
         else /* else weight pr */
         {
-            string weight_update_string = weight_pr.Text.ToString();
-            int weight_update = int.Parse(weight_update_string);
+            string weight_update_string = weight_pr.Text;
 
-            await App.RecordRepo.Update_PR(name, date, weight_update, -1, -1, -1);
+            if (weight_update_string != null && weight_update_string.Length != 0) /* if weight field is not empty */
+            {
+                weight_update_string = weight_update_string.ToString();
+
+                int weight_update = int.Parse(weight_update_string);
+                await App.RecordRepo.Update_PR(name, date, weight_update, -1, -1, -1);
+
+                error_prompt.IsVisible = false;
+                Close();
+            }
+            else /* if weight field is empty */
+            {
+                error_prompt.Text = "Weight value cannot be empty";
+                error_prompt.IsVisible = true;
+            }
         }
-
-        Close();
     }
 
     /* closes popup for creating an exercise */
