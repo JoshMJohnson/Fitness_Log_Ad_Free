c56cdc4b633f0ae9bc840317b41b7ede435f908b
JoshMJohnson <joshjohns21@gmail.com>
2023-07-23 15:15:15 -0500

    shell header implemented

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -5,10 +5,17 @@ public partial class AppShell : Shell
 	public AppShell()
 	{
 		InitializeComponent();
-		Prepare_Footer();
+		Prepare_Header();
+        Prepare_Footer();
     }
 
-	/* todo prepare footer */
+	/* prepare header */
+	private void Prepare_Header()
+	{
+		header_image.BackgroundColor = Color.FromRgb(153,217,234);
+    }
+
+	/* prepare footer */
 	private void Prepare_Footer()
 	{
 		/* time stamp */
