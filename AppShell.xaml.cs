8e9a1b81112969785f78ed882a4b5adeae9591b3
JoshMJohnson <joshjohns21@gmail.com>
2023-07-23 15:06:16 -0500

    shell footer implemented

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -5,5 +5,16 @@ public partial class AppShell : Shell
 	public AppShell()
 	{
 		InitializeComponent();
-	}
+		Prepare_Footer();
+    }
+
+	/* todo prepare footer */
+	private void Prepare_Footer()
+	{
+		/* time stamp */
+		DateTime current_date = DateTime.Now;
+		string[] date_string = current_date.ToString().Split(' ');
+		string date_display = date_string[0];
+		footer_date.Text = date_display;
+    }
 }
