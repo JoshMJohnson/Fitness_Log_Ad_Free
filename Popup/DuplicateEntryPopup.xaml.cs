e70c03a41c096ceda5d72ae0abbf4057abd62d61
JoshMJohnson <joshjohns21@gmail.com>
2023-07-30 17:45:55 -0500

    duplicate popup code adjustment for flexible table entries

--- Popup/DuplicateEntryPopup.xaml.cs
+++ Popup/DuplicateEntryPopup.xaml.cs
@@ -1,3 +1,5 @@
+using Android.Media.TV;
+
 namespace WorkoutLog.Popup;
 
 public partial class DuplicateEntryPopup
@@ -5,20 +7,27 @@ public partial class DuplicateEntryPopup
 	public DuplicateEntryPopup(string table_name)
 	{
 		InitializeComponent();
-		Display_Info(table_name);
+
+		/* if duplicate entry is a body progression entry */
+		if (table_name == "Body Progression")
+		{
+			Display_Body_Progression(table_name);
+        }
+
     }
 
-	/* fills in popup display */
-	private void Display_Info(string table_name)
+	/* displays body progressin duplicate info */
+	private void Display_Body_Progression(string table_name)
 	{
         duplicate_table_display.Text = table_name;
 
-		string prompt = $"The {table_name} entry is a duplicate.";
-		string prompt2 = $"The {table_name} entry was not added as a duplicate.";
+        string prompt = $"The {table_name} entry is a duplicate.";
+        string prompt2 = $"The {table_name} entry was not added as a duplicate.";
 
         duplicate_table_prompt_display.Text = prompt;
         duplicate_table_prompt2_display.Text = prompt2;
     }
+
 
 	/* closes the popup alert for duplicate */
 	private void Close_Popup(object sender, EventArgs e)
